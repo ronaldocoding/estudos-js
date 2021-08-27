bcb5b2e1c5c9bdda1c35549912539e5f26abc709
Ronaldo Costa <devronaldo.costa@gmail.com>
2021-08-26 22:58:39 -0400

    Objeto Math em js

---
+++ fundamentos/math.js
@@ -0,0 +1,16 @@
+/**
+ * O Javascript oferece um objeto que fornece funcionalidades e constantes 
+ * básicas de Matemática chamado "Math".
+ * 
+ * Abaixo é usada a constante "Math.PI" que retorna o número pi e a função
+ * "Math.pow()" que realiza operações de exponenciação a partir de dois números, 
+ * sendo o primeiro número a base e o segundo o expoente.
+ */
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+
+console.log(area.toFixed(2))
+
+//Output
+//98.54
