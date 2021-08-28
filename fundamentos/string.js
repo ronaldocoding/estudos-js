0a7295ad1233c44d1eeb3d35d478aa7e4054c8de
Ronaldo Costa <devronaldo.costa@gmail.com>
2021-08-27 22:46:46 -0400

    Strings em js

---
+++ fundamentos/string.js
@@ -0,0 +1,8 @@
+/**
+ * Uma string é uma cadeia de caracteres. Em Javascript pode-se delimitar uma
+ * string com aspas duplas, aspas simples, crases
+ */
+
+let nome1 = "Maria" //delimitação com aspas duplas
+let nome2 = 'João' //delimitação com aspas simples
+let nome3 = `Pedro` //delimitação com crases
