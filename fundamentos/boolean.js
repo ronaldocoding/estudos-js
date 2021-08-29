e6eb3e165e913d0c1acdf327535eeddfd40181b2
Ronaldo Costa <devronaldo.costa@gmail.com>
2021-08-28 23:34:24 -0400

    Correcoes boolean.js

--- fundamentos/boolean.js
+++ fundamentos/boolean.js
@@ -1,9 +1,9 @@
 /**
- * O tipo boolean serve para armazenar valores verdadeiros e falsos para
+ * O tipo boolean serve para armazenar valores verdadeiros ou falsos para
  * facilitar verificações na programação.
  * 
  * Diferentemente de outras linguagens, o Javacript não suporta apenas true ou
- * false para definir constantes e variáveis do tipo boolean.
+ * false para se usar em contextos lógicos.
  */
 
 /**
@@ -19,23 +19,24 @@ isAtivo = true
 console.log(isAtivo)
 
 /**
- * Nesse caso o número 1 representa um valor verdadeiro. Em seguida é usado o
- * operador da negação da linguagem, representado por uma exclamação "!". A
- * negação serve para verdadeiro se um valor é "não verdadeiro", ou seja falso e
- * "não falso", ou seja verdadeiro. 
+ * Nesse caso o número 1 representa um valor verdadeiro. Depois é usado o
+ * operador de negação da linguagem, representado por uma exclamação "!". A
+ * negação serve para verificar se um valor é "não verdadeiro", ou seja falso,
+ * "não falso", ou seja verdadeiro e vice-versa.
  * 
  * Além disso, o operador de negação também tem propriedade acumulativa. Por
- * exemplo, para se verificar se um valor é verdadeiro pode se escrever
+ * exemplo, para se verificar se um valor é verdadeiro pode-se escrever
  * "!!valor" e assim por diante.
  */
 isAtivo = 1
 console.log(!isAtivo) // "não verdadeiro" -> falso
 console.log(!!isAtivo) // "não não verdadeiro" -> verdadeiro
 
+
 /**
  * Como pode-se ver abaixo, o número 0 representa um valor falso. Sendo assim, 
- * todos os números inteiros, em contextos de uso de verdadeiro ou falso,
- * representam valores verdadeiros com exceção do 0.
+ * todos os números inteiros, em contextos lógicos, representam valores
+ *  verdadeiros com exceção do 0.
  */
 isAtivo = 0
 console.log(!!isAtivo) //"não não falso" -> falso
@@ -72,8 +73,10 @@ console.log(!!undefined)
 console.log(!!(isAtivo = false))
 
 /**
- * Usou do operador OU, "||". O operador lógico serve para verficar se ao menos
- * um valor em uma expressão é verdadeiro.
+ * Usou do operador OU, "||". 
+ * 
+ * O operador lógico OU serve para verificar se ao menos
+ * um valor em uma expressão lógica é verdadeiro.
  */
 
 /**
@@ -84,8 +87,11 @@ console.log(!!('' || null || 0 || 'verdadeiro'))
 
 /**
  * Em Javascript, ao usar o operador lógico OU em uma expressão da forma como
- * está abaixo resulta, se houver, o único valor verdadeiro.
+ * está abaixo resulta, se houver, o primeiro valor verdadeiro.
  * 
  * Repare que não foi usado o operador de negação.
  */
 console.log('' || null || 0 || 'verdadeiro' || 'falso')
+
+//Output
+//verdadeiro
