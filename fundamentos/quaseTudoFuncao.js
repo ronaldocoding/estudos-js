fc242bf8410c996dd18a464297569a6a77202b27
Ronaldo Costa <devronaldo.costa@gmail.com>
2021-09-01 23:08:49 -0400

    Quase tudo eh funcao em js

---
+++ fundamentos/quaseTudoFuncao.js
@@ -0,0 +1,16 @@
+/**
+ * Se Javascript fosse um país, a função seria seu presidente, o objeto seu
+ * vice e o array um de seus ministros talvez. Como veremos abaixo, quase tudo é
+ * função em Javascript, é o centro da linguagem.
+ */
+
+console.log(typeof Object)
+
+//Output
+//function
+
+class Produto {}
+console.log(typeof Produto)
+
+//Output
+//function
