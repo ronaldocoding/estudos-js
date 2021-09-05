d0885ee5bedcb9f5081cb2a6b8c8c26b5b754320
Ronaldo Costa <devronaldo.costa@gmail.com>
2021-09-04 22:55:12 -0400

    Correcoes usandoVar.js

--- fundamentos/usandoVar.js
+++ fundamentos/usandoVar.js
@@ -14,8 +14,11 @@
 
 console.log(sera)
 
+//Output
+//Será:??
+
 /**
- * No entanto, quando se declara uma varíavel dentro de uma função, mesmo uma
+ * No entanto, quando se declara uma variável dentro de uma função, mesmo uma
  * declarada com "var", o seu escopo é limitado a esse bloco de código, não
  * podendo ser acessada fora dele.
  */
@@ -32,8 +35,8 @@ teste()
  * Dessa forma, se uma variável for declarada com "var" fora de uma função, ela
  * se torna uma variável global, podendo ser acessada em qualquer lugar do 
  * código. Isso pode se tornar um problema em programas grandes, já que o
- * programador pode acabar sobrescrevendo uma varíavel importante e nem vai
+ * programador pode acabar sobrescrevendo uma variável importante e nem vai
  * saber disso, visto que o Javascript não vai apontar erro.
  * 
- * Amanha vou fazer uma thread explicando porque usar "let" é melhor.
+ * Amanhã vou fazer uma thread explicando porque usar "let" é melhor.
  */
